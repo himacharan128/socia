9bf1a2d03aef5bed1414738a9cb8796f62d15cf9
HimaCharan <himacharan@HCs-MacBook-Air.local>
2022-09-21 23:02:40 +0530

    header_done

---
+++ components/Header.js
@@ -0,0 +1,34 @@
+import Image from "next/image"
+import {
+    SearchIcon,PlusCircleIcon,UserGroupIcon,PaperAirplaneIcon,MenuIcon
+} from "@heroicons/react/outline"
+function Header(){
+    return(
+        <div>
+            <div className="flex justify-between max-w-6xl">
+                {/* left */}
+                <div className="relative hidden sm:inline-grid w-24 cursor-pointer">
+                    <Image src="https://links.papareact.com/ocw" layout="fill" objectFit="contain"/>
+                </div>
+                <div className="relative w-10 sm:hidden flex-shrink-0 cursor-pointer">
+                <Image src="https://links.papareact.com/jjm" layout="fill" objectFit="contain"/>
+                </div>
+                {/* middle */}
+                <div className="relative mt-1 p-1 rounded-md">
+                    <div className="absolute inset-y-0 pl-3 flex items-center pointer-events-none">
+                        <SearchIcon className="h-5 w-5 text-gray-500"/>
+                    </div>
+                    <input className="bg-gray-50 block w-full pl-10 sm:text-sm border-gray-300
+                     focus:ring-black focus:border-black rounded-md" 
+                     type="text" placeholder="search"/>
+                </div>
+                {/* right */}
+                <div>
+                    <PlusCircleIcon/>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Header
