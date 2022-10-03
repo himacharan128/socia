c1016a6370bee606c4184636d704f38751a4eeb9
HimaCharan <himacharan@HCs-MacBook-Air.local>
2022-10-04 00:58:28 +0530

    auth_done

--- components/Suggestions.js
+++ components/Suggestions.js
@@ -1,58 +1,46 @@
+const profile=[
+    {
+        id:'user_id',
+        avatar:'https://i.ibb.co/x64jpHG/View-recent-photos.jpg',
+        username:'user_name',
+        caption:'caption....................................................!',
+    },
+    {
+        id:'user_id',
+        avatar:'https://i.ibb.co/x64jpHG/View-recent-photos.jpg',
+        username:'user_name',
+        caption:'caption....................................................!',
+    },
+    {
+        id:'user_id',
+        avatar:'https://i.ibb.co/x64jpHG/View-recent-photos.jpg',
+        username:'user_name',
+        caption:'caption....................................................!',
+    },
+    {
+        id:'user_id',
+        avatar:'https://i.ibb.co/x64jpHG/View-recent-photos.jpg',
+        username:'user_name',
+        caption:'caption....................................................!',
+    },
+]
 function Suggestions(){
     return(
         <div className="mt-4 ml-10">
-            <div className="flex justify-between text-sm mb-5">
+            <div  key={profile.id} className="flex justify-between text-sm mb-5">
                 <h3 className="text-sm font-bold text-gray-400">Suggestions for you</h3>
                 <button className="text-gray-600 font-semibold">See All</button>
             </div>
+        {profile.map((profile)=> ( 
             <div className="flex items-center justify-between mt-3">
-                <img className="w-10 h-10 border p-[2px] rounded-full" alt="" src="https://i.ibb.co/x64jpHG/View-recent-photos.jpg"/>
+                <img className="w-10 h-10 border p-[2px] rounded-full" alt="" src={profile.avatar}/>
                 <div className="flex-1 ml-4">
-                    <h2 className="text-sm font-semibold">USERNAME</h2>
-                    <h3 className="text-xs text-gray-400">CAPTION....................!</h3>
+                    <h2 className="text-sm font-semibold">{profile.username}</h2>
+                    <h3 className="text-xs text-gray-400">{profile.caption}</h3>
                 </div>
                 <button className="text-blue-400 text-sm font-bold">FOLLOW</button>
             </div>
-            <div className="flex items-center justify-between mt-3">
-                <img className="w-10 h-10 border p-[2px] rounded-full" alt="" src="https://i.ibb.co/x64jpHG/View-recent-photos.jpg"/>
-                <div className="flex-1 ml-4">
-                    <h2 className="text-sm font-semibold">USERNAME</h2>
-                    <h3 className="text-xs text-gray-400">CAPTION....................!</h3>
-                </div>
-                <button className="text-blue-400 text-sm font-bold">FOLLOW</button>
-            </div>
-            <div className="flex items-center justify-between mt-3">
-                <img className="w-10 h-10 border p-[2px] rounded-full" alt="" src="https://i.ibb.co/x64jpHG/View-recent-photos.jpg"/>
-                <div className="flex-1 ml-4">
-                    <h2 className="text-sm font-semibold">USERNAME</h2>
-                    <h3 className="text-xs text-gray-400">CAPTION....................!</h3>
-                </div>
-                <button className="text-blue-400 text-sm font-bold">FOLLOW</button>
-            </div>
-            <div className="flex items-center justify-between mt-3">
-                <img className="w-10 h-10 border p-[2px] rounded-full" alt="" src="https://i.ibb.co/x64jpHG/View-recent-photos.jpg"/>
-                <div className="flex-1 ml-4">
-                    <h2 className="text-sm font-semibold">USERNAME</h2>
-                    <h3 className="text-xs text-gray-400">CAPTION....................!</h3>
-                </div>
-                <button className="text-blue-400 text-sm font-bold">FOLLOW</button>
-            </div>
-            <div className="flex items-center justify-between mt-3">
-                <img className="w-10 h-10 border p-[2px] rounded-full" alt="" src="https://i.ibb.co/x64jpHG/View-recent-photos.jpg"/>
-                <div className="flex-1 ml-4">
-                    <h2 className="text-sm font-semibold">USERNAME</h2>
-                    <h3 className="text-xs text-gray-400">CAPTION....................!</h3>
-                </div>
-                <button className="text-blue-400 text-sm font-bold">FOLLOW</button>
-            </div>
-            <div className="flex items-center justify-between mt-3">
-                <img className="w-10 h-10 border p-[2px] rounded-full" alt="" src="https://i.ibb.co/x64jpHG/View-recent-photos.jpg"/>
-                <div className="flex-1 ml-4">
-                    <h2 className="text-sm font-semibold">USERNAME</h2>
-                    <h3 className="text-xs text-gray-400">CAPTION....................!</h3>
-                </div>
-                <button className="text-blue-400 text-sm font-bold">FOLLOW</button>
-            </div>
+        ))}
         </div>
         );
     }
---
+++ firebase.js
@@ -0,0 +1,27 @@
+// Import the functions you need from the SDKs you need
+import { getApp, getApps, initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+import { getStorage } from "firebase/storage";
+
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyCA8ULK5fn_7gxKnYtxlLqMGUxUBaKZXPQ",
+  authDomain: "socia-6389a.firebaseapp.com",
+  projectId: "socia-6389a",
+  storageBucket: "socia-6389a.appspot.com",
+  messagingSenderId: "717134017380",
+  appId: "1:717134017380:web:7ca3fc52642d4b09188071",
+  measurementId: "G-REBGMPHZ0T"
+};
+
+// Initialize Firebase
+const app = !getApps().length ? initializeApp(firebaseConfig): getApp;
+const analytics = getAnalytics(app);
+const db = getFirestore();
+const storage= getStorage();
+
+export{ app,db,storage};
