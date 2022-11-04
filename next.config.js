b2a1cb1c526d898e65781450d1f3984701440d86
HimaCharan <himacharan@HCs-MacBook-Air.local>
2022-11-05 00:31:42 +0530

    deploy_test3

--- next.config.js
+++ next.config.js
@@ -3,4 +3,9 @@ module.exports={
     domains:["links.papareact.com","i.ibb.co","ibb.co","lh3.googleusercontent.com",
   "firebasestorage.googleapis.com"],
   },
+  eslint: {
+    // Warning: This allows production builds to successfully complete even if
+    // your project has ESLint errors.
+    ignoreDuringBuilds: true,
+  },
 };
