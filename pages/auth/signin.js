8ab90cf476dff57fa43f114a68d74a4418dcea01
HIMA CHARAN GANGULA <himacharan128@gmail.com>
2022-11-08 23:23:58 +0530

    sign in page_updated

--- pages/auth/signin.js
+++ pages/auth/signin.js
@@ -5,21 +5,29 @@ function SignIn({providers}){
     // console.log(providers)
     return (
         <>
-        <Header/>
-        <div className=" flex flex-col items-center justify-center min-h-screen
-        py-2 -mt-56 px-14 text-center">
-
-        <div className="mt-40">
-        <img className="w-80 rounded " alt="" src="https://i.ibb.co/n359qnG/Screenshot-2022-11-04-at-11-17-10-PM.png"/>
-        {Object.values(providers).map((provider) => (
-            <div key={provider.name}>
-                <button className="mt-10 p-4 bg-blue-500 rounded-lg text-white" 
-                onClick={() => SignIntoProvider(provider.id,{callbackUrl:'/'})}>
-                    Sign in with {provider.name}
-                </button>
+        <div className="bg-black">
+            <div className=" flex flex-col items-center justify-center min-h-screen
+            py-2 mt px-14 text-center">
+            <div className="mt-40">
+            <img className=" ml-20 w-36 rounded " alt="" src="https://i.ibb.co/DKNDjJJ/image.jpg"/>
+            <img className="w-80 rounded mt-10" alt="" src="https://i.ibb.co/XZfCLsp/image.png"/>
+            {Object.values(providers).map((provider) => (
+                <div key={provider.name}>
+                    <button className="mt-10 p-4 bg-blue-500 rounded-lg text-white" 
+                    onClick={() => SignIntoProvider(provider.id,{callbackUrl:'/'})}>
+                        Sign in with {provider.name}
+                    </button>
+                </div>
+            ))}
             </div>
-        ))}
-        </div>
+            <div className="p-4 bg-slate-100 rounded-xl px-4 shadow-sm mt-52">
+            <h1>
+            No login password credentials are stored in our database, they are completely authorized
+            and authenticated by Google Services.
+            </h1>
+    
+            </div>
+            </div>
         </div>
         </>
     );
